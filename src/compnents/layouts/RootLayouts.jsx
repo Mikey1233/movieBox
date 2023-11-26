057b36e719f89d70f5e5dc707bd016f1ba141c7c
michael1233 <emmamichael201@gmail.com>
2023-11-26 02:04:46 +0100

    css changes

--- src/compnents/layouts/RootLayouts.jsx
+++ src/compnents/layouts/RootLayouts.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink,Outlet } from 'react-router-dom'
+import img from '../../assets/image-avatar.png'
 import './RootLayouts.css'
 import logo from '../../assets/monero.svg'
 function RootLayouts() {
@@ -9,13 +10,23 @@ function RootLayouts() {
       <nav>
         <div className='root-layouts__img'><img src={logo}/></div>
         <ul>
-        <NavLink to={'/'}>All</NavLink>
-        <NavLink to={'movies'}>Movies</NavLink>
-        <NavLink to={'tv-series'}>Tv-series</NavLink>
-        <NavLink to={'book-marks'}>Bookmarks</NavLink>
+        <NavLink to={'/'}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-border-all" viewBox="0 0 16 16">
+  <path d="M0 0h16v16H0zm1 1v6.5h6.5V1zm7.5 0v6.5H15V1zM15 8.5H8.5V15H15zM7.5 15V8.5H1V15z"/>
+</svg></NavLink>
+        <NavLink to={'movies'}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
+  <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z"/>
+</svg></NavLink>
+        <NavLink to={'tv-series'}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tv" viewBox="0 0 16 16">
+  <path d="M2.5 13.5A.5.5 0 0 1 3 13h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5M13.991 3l.024.001a1.46 1.46 0 0 1 .538.143.757.757 0 0 1 .302.254c.067.1.145.277.145.602v5.991l-.001.024a1.464 1.464 0 0 1-.143.538.758.758 0 0 1-.254.302c-.1.067-.277.145-.602.145H2.009l-.024-.001a1.464 1.464 0 0 1-.538-.143.758.758 0 0 1-.302-.254C1.078 10.502 1 10.325 1 10V4.009l.001-.024a1.46 1.46 0 0 1 .143-.538.758.758 0 0 1 .254-.302C1.498 3.078 1.675 3 2 3zM14 2H2C0 2 0 4 0 4v6c0 2 2 2 2 2h12c2 0 2-2 2-2V4c0-2-2-2-2-2"/>
+</svg></NavLink>
+        <NavLink to={'book-marks'}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+  <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+</svg>
+</NavLink>
         </ul>
        <div>
-       <NavLink to={'account'}>Account</NavLink>
+       <NavLink to={'account'}><img src={img} alt='avatar' className='avatar'/></NavLink>
        </div>
         
 
--- src/compnents/pages/all/All.jsx
+++ src/compnents/pages/all/All.jsx
@@ -3,13 +3,17 @@ import './All.css'
 import MovieBox from '../../movieBox/MovieBox'
 import { useLoaderData } from 'react-router-dom'
 function All() {
-  // const data = useLoaderData()
-  // console.log(data)
+  const data = useLoaderData()
+  console.log(data.results)
+  //trending movie 
+  const trending = data.results
   return (
     <div className='movieBox-all'>
       <div className='movieBox-all-input'>
         <input type='text' placeholder='Search for movie or Tv-series'/>
-        
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+</svg>
       </div>
       <div className='movieBox-all-content'>
         <h2>Trending</h2>
@@ -52,9 +56,20 @@ function All() {
 }
 
 export default All
-
+//78393d09e7d06dc8a1d807120b3c221e
 ////////////loader func
-// export const loaderData = async ()=>{
- 
+export const loaderData = async ()=>{
+  const options = {
+    method: 'GET',
+  };
   
-// }
+const data = await fetch( 'https://api.themoviedb.org/3/trending/all/day?api_key=78393d09e7d06dc8a1d807120b3c221e', options)
+if (!data.ok) {
+  throw Error('Could not find that career.')
+}
+  return data.json()
+}
+
+
+// fetch(language=en-US', options)
+  
