e8d00ae789c543dbab8c15e7c337117c95ade7e8
michael1233 <emmamichael201@gmail.com>
2023-12-26 01:18:09 +0100

    added firebase files to the project

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxzNZxdaLYkbnLyS_xlUreVfdou7Y3M-o",
+  authDomain: "moviesite-a9253.firebaseapp.com",
+  projectId: "moviesite-a9253",
+  storageBucket: "moviesite-a9253.appspot.com",
+  messagingSenderId: "699180012360",
+  appId: "1:699180012360:web:8021fdbf343f791200197d",
+  measurementId: "G-7VL0LZ5JC4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider();
